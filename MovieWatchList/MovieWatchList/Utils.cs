6d1bba4f729e6e7c82e0c0ebb9fe42f62fe666e9
RICHARD COOKE - Student <G00331787@gmit.ie>
2019-04-10 17:03:54 +0100

    first commit

---
+++ MovieWatchList/MovieWatchList/Utils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MovieWatchList;
+using Newtonsoft.Json;
+
+namespace Utils
+{
+    class MyUtils
+    {
+        public const string JSONFILE = "Movies.txt";
+    }
+}
