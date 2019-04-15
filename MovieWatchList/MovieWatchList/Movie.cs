d17b465b1d9ec2c648b0508c9ac8ec146cc463a6
RICHARD COOKE - Student <G00331787@gmit.ie>
2019-04-15 17:24:54 +0100

    Updated single view

---
+++ MovieWatchList/MovieWatchList/Movie.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieWatchList
+{
+    class Movie
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Year { get; set; }
+        public string RunTime { get; set; }
+        public string Country { get; set; }
+        public string IMDBRating { get; set; }
+        public string Starring { get; set; }
+        public string Director { get; set; }
+        public string BoxOffice { get; set; }
+
+        //Constructors
+        public Movie()
+        {
+
+        }
+
+        public Movie(string t, string y, string d, string rt, string c, string ir, string s, string dir, string b)
+        {
+            Title = t;
+            Description = d;
+            Year = y;
+            RunTime = rt;
+            Country = c;
+            IMDBRating = ir;
+            Starring = s;
+            Director = dir;
+            BoxOffice = b;
+        }
+    }
+}
